c91c9f571463713388695754fb8333743ba2f16c
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2021-03-25 11:13:27 +1100

    progress
    a bunch of stuff, im learning tkinter
    also made some critical sets & dicts for build creation.

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# import libs
+import tkinter as tk
+
+# import lists, sets, dicts, etc
+import staticvars as sv
+
+# create main window
+mainWindow = tk.Tk()
+mainWindow.title("Skyrim Build Journal")
+
+# mainloop
+mainWindow.mainloop()
---
+++ src/staticvars.py
@@ -0,0 +1,25 @@
+raceSet = {"High Elf", "Argonian", "Wood Elf", "Breton", "Dark Elf", "Imperial", "Khajiit", "Nord", "Orc", "Redguard"}
+sexSet = {"Male", "Female"}
+attributeSet = {"Magicka", "Stamina", "Health"}
+primaryPerkDict = {
+    "The Mage": {
+        "Illusion", "Conjuration", "Destruction", "Restoration", "Alteration"
+    },
+    "The Warrior": {
+        "Heavy Armor", "Block", "Two-Handed", "One-Handed", "Archery"
+    },
+    "The Thief": {
+        "Light Armor", "Sneak"
+    }
+}
+secondaryPerkDict = {
+    "The Mage": {
+        "Enchanting"
+    },
+    "The Warrior": {
+        "Smithing"
+    },
+    "The Thief": {
+        "Lockpicking", "Pickpocket", "Speech", "Alchemy"
+    }
+}
